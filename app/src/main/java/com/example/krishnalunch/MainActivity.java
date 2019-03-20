503d7b6eeb75f0c94101fdea20635ef2795200a8
Marco Austria <maustria0507@gmail.com>
2019-03-19 22:20:14 -0400

    final first round high fidelity

--- app/src/main/java/com/example/krishnalunch/MainActivity.java
+++ app/src/main/java/com/example/krishnalunch/MainActivity.java
@@ -2,16 +2,41 @@ package com.example.krishnalunch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.weekly_menu);
+
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+
+        Spinner spinner =findViewById(R.id.spinner);
+        ArrayAdapter adapter=ArrayAdapter.createFromResource(
+                this,
+        R.array.weekday,
+        R.layout.spinner_item
+        );
+        adapter.setDropDownViewResource(R.layout.spinner_dropdown);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
     }
 
 
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+        Toast.makeText(this, adapterView.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
 }
