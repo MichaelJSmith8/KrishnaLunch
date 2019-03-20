3b5c653cbfbc1084c1fb1375f6b4eb025b9674bd
bmottsmith <bmottsmith@gmail.com>
2019-03-20 00:34:13 -0400

    Added Splash screen (log in and sign up), log in screen, and sign up screen
    Also removed toolbar from styles.xml, switched to LogInSignUpAction as first action in AndroidManifest.xml
    
    Added krishna_logo to drawable

--- app/src/main/java/com/example/krishnalunch/LogIn.java
+++ app/src/main/java/com/example/krishnalunch/LogIn.java
@@ -5,7 +5,6 @@ import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -16,22 +15,11 @@ public class LogIn extends AppCompatActivity {
 
     ImageView KrishnaLogo;
     TextView DirectToSignUpText;
-    Button LogInButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
-
-        LogInButton =(Button)findViewById(R.id.LogInBtn);
-
-        LogInButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
-            }
-        });
 
         KrishnaLogo=(ImageView)findViewById(R.id.KrishnaLogo);
 
