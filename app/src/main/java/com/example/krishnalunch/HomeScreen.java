880c8c70dc0d008649cafed4fe21d30e33715ae8
gsansone4 <gsansone4@gmail.com>
2019-03-26 18:04:48 -0400

    Buy More and Redeem work (simple +/- on click)

--- app/src/main/java/com/example/krishnalunch/HomeScreen.java
+++ app/src/main/java/com/example/krishnalunch/HomeScreen.java
@@ -6,33 +6,66 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
 
 public class HomeScreen extends AppCompatActivity {
 
     TextView counter;
     Button BuyMore;
     FloatingActionButton redeem;
-    int count;
+    int count = 0;
+
+    private View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch(v.getId()){
+                case R.id.BuyMore:
+                    addTicket();
+                    break;
+                case R.id.redeem:
+                    redeemTicket();
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = findViewById(R.id.toolbar2);
+        //setSupportActionBar(toolbar);
 
         counter = findViewById(R.id.counter);
+        initCount();
         BuyMore = findViewById(R.id.BuyMore);
-        redeem = findViewById(R.id.redeem);
+        BuyMore.setOnClickListener(listener);
 
-        /*FloatingActionButton fab = findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.redeem);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                redeemTicket();
             }
-        });*/
+        });
+    }
+
+    private void initCount(){
+        // Note: should pull ticket number from account/db here
+        count = 0;
+        counter.setText(String.valueOf(count));
+    }
+
+    private void addTicket(){
+        count++;
+        counter.setText(String.valueOf(count));
+    }
+
+    private void redeemTicket(){
+        if (count > 0)
+            count--;
+        counter.setText(String.valueOf(count));
     }
 
 }
